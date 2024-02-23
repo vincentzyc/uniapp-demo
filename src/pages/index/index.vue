28d8ca6866f2657ce5adfa1c18ac4b78c53c481c
vincentzyc <vincentzyc@qq.com>
2024-02-23 18:07:53 +0800

    🎉init: init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <view class="content">
+    <image class="logo" src="/static/logo.png" />
+    <view class="text-area">
+      <text class="title">{{ title }}</text>
+    </view>
+    <view>
+      <text class="title">测试</text>
+    </view>
+    <uni-badge text="1"></uni-badge>
+    <uni-badge text="2" type="success"></uni-badge>
+    <uni-badge text="3" type="primary" :inverted="true"></uni-badge>
+    <uni-card :is-shadow="false" is-full>
+			<text class="uni-h6">卡片组件通用来显示完整独立的一段信息，同时让用户理解他的作用。例如一篇文章的预览图、作者信息、时间等，卡片通常是更复杂和更详细信息的入口点。</text>
+		</uni-card>
+		<uni-section title="基础卡片" type="line">
+			<uni-card :is-shadow="false">
+				<text class="uni-body">这是一个基础卡片示例，内容较少，此示例展示了一个没有任何属性不带阴影的卡片。</text>
+			</uni-card>
+		</uni-section>
+  </view>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+const title = ref('Hello');
+</script>
+
+<style>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.logo {
+  height: 200rpx;
+  width: 200rpx;
+  margin-top: 200rpx;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 50rpx;
+}
+
+.text-area {
+  display: flex;
+  justify-content: center;
+}
+
+.title {
+  font-size: 36rpx;
+  color: #8f8f94;
+}
+</style>
