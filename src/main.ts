8e31a70ceee82492b46a3099fbe1c93b3d9696c5
vincentzyc <vincentzyc@qq.com>
2024-03-02 19:39:00 +0800

    🎉 init: init

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import { createSSRApp } from "vue";
+import VueLazyload from 'vue-lazyload'  
+import './assets/css/main.scss'
 import App from "./App.vue";
 export function createApp() {
   const app = createSSRApp(App);
+  app.use(VueLazyload, {
+    preLoad: 1.0,
+    // error: 'dist/error.png',
+    // loading: 'dist/loading.gif',
+    attempt: 1
+  });
   return {
     app,
   };
