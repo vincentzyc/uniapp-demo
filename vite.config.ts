59b5a911c72defe5c3278c26abc3b0a61ebd09de
vincentzyc <vincentzyc@qq.com>
2024-03-04 11:27:55 +0800

    🎉init: init

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from "vite";
-import uni from "@dcloudio/vite-plugin-uni";
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vite';
+import uni from '@dcloudio/vite-plugin-uni';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [uni()],
+  test: {
+    globals: true,
+  },
 });
