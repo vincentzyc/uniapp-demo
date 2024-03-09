768fc09309a7d463fe639c5581fe72a820e47dbf
vincentzyc <vincentzyc@qq.com>
2024-03-09 16:31:43 +0800

    🎉init: init

--- utils/__tests__/validate/date.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test,expect } from 'vitest';
-import { isDate } from '../../validate/date'
-
-test('date', () => {
-  const date1 = new Date()
-  const date2 = new Date('2022-01-01')
-  expect(isDate(date1)).toBe(true);
-  expect(isDate(date2)).toBe(true);
-})
--- utils/validate/date.ts
+++
@@ -1,6 +0,0 @@
-import { isNaN } from './number';
-import { getType } from './data-type';
-
-export function isDate(val: Date): val is Date {
-  return getType(val) === 'Date' && !isNaN(val.getTime());
-}
