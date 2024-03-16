f4a967a69224c2c5623a5cf703ebbb25457d344a
vincentzyc <vincentzyc@qq.com>
2024-03-16 22:25:14 +0800

    🎉 init: init

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,17 @@
 
 import { defineConfig } from 'vite';
 import uni from '@dcloudio/vite-plugin-uni';
+import postcssPxtorpx from './postcss-pxtorpx.ts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [uni()],
   test: {
     globals: true,
+  },
+  css: {
+    postcss: {
+      plugins: [postcssPxtorpx()],
+    },
   },
 });
