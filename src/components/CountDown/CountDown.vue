d2e87ec421495cd82de4544427a57c7fffed7965
vincentzyc <vincentzyc@qq.com>
2024-03-25 19:26:39 +0800

    🎉 init: init

--- src/components/CountDown/CountDown.vue
+++ src/components/CountDown/CountDown.vue
@@ -10,13 +10,30 @@
 import { ref } from 'vue';
 import './style.scss';
 
-const testHour = ref(1),
+const testHour = ref(0),
   testMinute = ref(0),
   testSecond = ref(0);
 
+// function getDayLoopEndTime() {
+//   // const today = new Date().toLocaleDateString();
+//   // const todayTimeStamp = new Date(today).getTime();
+//   // return todayTimeStamp + 24 * 60 * 60 * 1000;
+//   // return hour * 60 * 60 + minute * 60 + second;
+// }
+
+// 获取当前时间距离0点的时分秒
 function getDayLoopEndTime() {
-  const today = new Date().toLocaleDateString();
-  const todayTimeStamp = new Date(today).getTime();
-  return todayTimeStamp + 24 * 60 * 60 * 1000;
+  const now = new Date();
+  const tomorrowMidnight = new Date(now);
+  tomorrowMidnight.setDate(now.getDate() + 1);
+  tomorrowMidnight.setHours(0, 0, 0, 0);
+  const timeDiff = Number(tomorrowMidnight) - Number(now);
+  const hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+  testHour.value = hours;
+  testMinute.value = minutes;
+  testSecond.value = seconds;
 }
+getDayLoopEndTime();
 </script>
