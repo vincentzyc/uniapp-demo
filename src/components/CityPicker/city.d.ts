8ce9879a027cd41971a57161fc489ec6f6074aa7
vincentzyc <vincentzyc@qq.com>
2024-04-25 19:40:58 +0800

    🎉 init: init

--- src/components/CityPicker/city.d.ts
+++ src/components/CityPicker/city.d.ts
@@ -1,6 +1,17 @@
+
+
 interface TYPE_CITY_ITEM {
   n: string;
   c?: TYPE_CITY_ITEM;
 }
 
-export type TYPE_CITY = TYPE_CITY_ITEM[];
+// export type TYPE_CITY = TYPE_CITY_ITEM[];
+
+
+// declare module './city' {
+//   export type TYPE_CITY = TYPE_CITY_ITEM[];
+// }
+
+declare module './city.js' {
+  export const CITY: TYPE_CITY_ITEM[];
+}
