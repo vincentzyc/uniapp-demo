f402195745438f5f72e3975c52d1e7a6d7e1e2dc
vincentzyc <vincentzyc@qq.com>
2024-04-26 17:24:50 +0800

    🎉 init: init

--- src/components/CityPicker/city.d.ts
+++ src/components/CityPicker/city.d.ts
@@ -1,17 +1,13 @@
-
+// import CITY from './city.js';
 
 interface TYPE_CITY_ITEM {
   n: string;
   c?: TYPE_CITY_ITEM;
 }
 
-// export type TYPE_CITY = TYPE_CITY_ITEM[];
+export type TYPE_CITY = TYPE_CITY_ITEM[];
 
+const CITY: TYPE_CITY;
 
-// declare module './city' {
-//   export type TYPE_CITY = TYPE_CITY_ITEM[];
-// }
-
-declare module './city.js' {
-  export const CITY: TYPE_CITY_ITEM[];
-}
+// export default TYPE_CITY;
+export default CITY;
