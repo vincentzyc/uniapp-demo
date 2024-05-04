8f6a9b6011c81636dbe6b87c69895d1fccc8b1dc
vincentzyc <vincentzyc@qq.com>
2024-05-04 22:01:08 +0800

    🎉init: init

--- src/components/FormItem/FormCity.vue
+++ src/components/FormItem/FormCity.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="flex align-middle form-item">
     <div class="form-label">收货城市</div>
-    <div class="form-input flex-auto flex align-middle">
+    <!-- <div class="form-input flex-auto flex align-middle">
       <input
         @click="openCityPicker()"
         placeholder="请选择收货城市"
         type="text"
         v-model.trim="model"
       />
-    </div>
-    <!-- <div @click="openCityPicker()" class="form-input flex-auto flex align-middle disabled-input"> -->
-    <!-- <div @click="openCityPicker()" class="form-input flex flex-auto align-middle disabled-input">
-      <div class="wg-input" :class="{ placeholder: !showValue }">{{ showValue ? showValue : "请选择收货城市" }}</div>
     </div> -->
+    <!-- <div @click="openCityPicker()" class="form-input flex-auto flex align-middle disabled-input"> -->
+    <div @click="openCityPicker()" class="form-input flex flex-auto align-middle disabled-input">
+      <div class="wg-input" :class="{ placeholder: !showValue }">{{ showValue ? showValue : "请选择收货城市" }}</div>
+    </div>
     <!-- <CityPicker @selected="closePicker" :locationCity="locationCity" ref="domCityPicker" v-model:show="showPicker" /> -->
     <CityPicker ref="domCityPicker" @change="pickCity"/>
   </div>
