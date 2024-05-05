493ae7858c17f86e06fc38ac07e8787041f8a861
vincentzyc <vincentzyc@qq.com>
2024-05-05 22:39:35 +0800

    🎉init: init

--- src/components/FormItem/FormPhone.vue
+++ src/components/FormItem/FormPhone.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="flex align-middle form-item">
-    <div class="form-label">联系电话</div>
-    <div class="form-input flex-auto flex align-middle">
+  <view class="flex align-middle form-item">
+    <view class="form-label">联系电话</view>
+    <view class="form-input flex-auto flex align-middle">
       <input
         @blur="checkPhone(model)"
         :maxlength="11"
@@ -9,31 +9,41 @@
         type="tel"
         v-model.trim="model"
       />
-    </div>
-  </div>
+    </view>
+  </view>
 </template>
 
 <script lang="ts">
-import { checkPhone } from "@/composition/business/useVerifyData"
+import { computed, defineComponent } from 'vue';
+// import { checkPhone } from "@/composition/business/useVerifyData"
 
 export default defineComponent({
-  name: "FormPhone",
+  name: 'FormPhone',
   props: {
     modelValue: {
       required: true,
       type: String,
     },
   },
-  emits: ["update:modelValue"],
+  emits: ['update:modelValue'],
   setup(props, { emit }) {
     const model = computed<string>({
       get() {
         return props.modelValue;
       },
       set(val) {
-        emit("update:modelValue", val);
+        emit('update:modelValue', val);
       },
     });
+    function isMobile(value: string): boolean {
+      value = value.replace(/[^-|\d]/g, '');
+      return /^((\+86)|(86))?1[3-9]\d{9}$/.test(value) || /^0[0-9-]{10,13}$/.test(value);
+    }
+    function checkPhone(value: string) {
+      if (!value) return '请输入联系电话';
+      if (isMobile(value)) return true;
+      return '请输入正确的手机号码';
+    }
     return {
       model,
       checkPhone,
