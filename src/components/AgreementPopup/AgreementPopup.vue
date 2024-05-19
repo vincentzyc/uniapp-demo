151d38e8aa9780efc9db115f19ad33aaf9c17868
vincentzyc <vincentzyc@qq.com>
2024-05-19 16:01:17 +0800

    🎉init: init

--- src/components/AgreementPopup/AgreementPopup.vue
+++ src/components/AgreementPopup/AgreementPopup.vue
@@ -2,9 +2,9 @@
   <uni-popup ref="popup" mask-background-color="rgba(0,0,0,0.6)" type="center" borderRadius="20px 20px 20px 20px"
     @maskClick="close" background-color="#fff">
     <view class="agreement-popup">
-      <text class="agreement-wrap">
+      <view class="agreement-wrap">
         <iframe :src="text" frameborder="0" style="width: 100%; height: 100%"></iframe>
-      </text>
+      </view>
       <text class="agreement-confirm" @click="close()">我知道了</text>
     </view>
   </uni-popup>
@@ -15,19 +15,11 @@ import { ref } from 'vue';
 import './style.scss';
 
 const props = defineProps<{
-  // show: boolean;
   text: string;
 }>();
-// const emits = defineEmits<{
-//   (e: 'update:show', bool: boolean): void;
-// }>();
 
 const popup = ref();
 
-// let isShow = computed({
-//   get: () => props.show,
-//   set: v => emits('update:show', v),
-// });
 function open() {
   popup.value.open();
 }
